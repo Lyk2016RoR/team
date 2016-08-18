29fac7f45d1d3ec7058e77a8e81a31b85306b5e2
tugba <tugbashn95@hotmail.com>
2016-08-18 14:10:49 +0300

    added some files

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   #devise_for :admins
   #devise_for :users
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
   }
   devise_for :admins, controllers: {
-      sessions: 'admins/sessions'
+      sessions: 'admins/sessions',
+      registrations: 'admins/registrations'
     }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
