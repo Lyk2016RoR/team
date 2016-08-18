8a74418eb2cbe7092946917a22cde2a7215bc850
tugba <tugbashn95@hotmail.com>
2016-08-18 11:51:01 +0300

    Added devise models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  #devise_for :admins
+  #devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+  devise_for :admins, controllers: {
+      sessions: 'admins/sessions'
+    }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
